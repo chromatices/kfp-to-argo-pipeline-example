bf71bd09518e3504641f6500ce58c03b23217bb2
chromatices <pjh403@naver.com>
2022-12-09 16:57:33 +0900

    update installing pipeline-runner stuffs

--- run-yaml.sh
+++ run-yaml.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 kubectl create namespace argo-test
-kubectl apply -f ./utils/argo_install.yaml -n argo-test
-kubectl apply -f https://raw.githubusercontent.com/kubeflow/manifests/5b1256f19a728908a7245db7460c3f742b01fb1e/apps/pipeline/upstream/base/pipeline/pipeline-runner-role.yaml -f https://raw.githubusercontent.com/kubeflow/manifests/5b1256f19a728908a7245db7460c3f742b01fb1e/apps/pipeline/upstream/base/pipeline/pipeline-runner-rolebinding.yaml -f https://raw.githubusercontent.com/kubeflow/manifests/5b1256f19a728908a7245db7460c3f742b01fb1e/apps/pipeline/upstream/base/pipeline/pipeline-runner-sa.yaml -n argo-test
+kubectl apply -f ./install/argo_install.yaml -n argo-test
+kubectl apply -f https://raw.githubusercontent.com/kubeflow/manifests/5b1256f19a728908a7245db7460c3f742b01fb1e/apps/pipeline/upstream/base/pipeline/pipeline-runner-role.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubeflow/manifests/5b1256f19a728908a7245db7460c3f742b01fb1e/apps/pipeline/upstream/base/pipeline/pipeline-runner-rolebinding.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubeflow/manifests/5b1256f19a728908a7245db7460c3f742b01fb1e/apps/pipeline/upstream/base/pipeline/pipeline-runner-sa.yaml -n argo-test
 kubectl create -f ./utils/pipeline-sample.yaml -n argo-test
